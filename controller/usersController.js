93d6d73a3ca0f1a59224db25f7eed714dd58a7ec
tortoisejr <mdsabbirh083@gmail.com>
2026-09-06 22:31:37 +0600

    add user in database

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,42 @@
-function getUsers(req,res,next){
-    res.render("user.ejs");
+const bcrypt = require("bcrypt");
+const { addUser } = requier("../lib/user/userCrud.js");
+
+function getUsers(req, res, next) {
+  res.render("user.ejs");
 }
 
-module.exports={
-    getUsers
+function addUsers(req, res, next) {
+  let userObj = {};
+  const hashedPassword = bcrypt.hash(req.body.password, 10);
+  if (req.files && req.files.length > 0) {
+    userObj = {
+      ...req.body,
+      password: hashedPassword,
+      avater: req.files[0].filename,
+    };
+  } else {
+    userObj = {
+      ...req.body,
+      password: hashedPassword,
+    };
+  }
+  addUser(userObj, (isErorr, err) => {
+    if (isErorr) {
+      res.status(500).json({
+        errors: {
+          common: {
+            msg: "Unknow error occured",
+          },
+        },
+      });
+    } else {
+      res.status(200).json({
+        message: "User was added successfully!",
+      });
+    }
+  });
 }
+
+module.exports = {
+  getUsers,
+};
